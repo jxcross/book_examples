1957900ca32cc025db7d2520c75ef8538414c9f1
jxcross <sghan73@naver.com>
2023-10-23 15:29:25 +0900

    231023-1529

---
+++ keycloak_test1/springboot/backend/src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,41 @@
+package com.example.springboot;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        trustAllHttpsCertificates();
+        SpringApplication.run(Application.class, args);
+    }
+
+    private static void trustAllHttpsCertificates() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            HostnameVerifier allHostsValid = (hostname, session) -> true;
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
